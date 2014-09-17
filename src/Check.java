8b27bb8765bf4b6ccb81f1075ce2212f2985bf7c
Yiming Zhang <steven.y.zh@hotmail.com>
2014-09-16 18:28:42 -0700

    check duplicated charactor

---
+++ src/Check.java
@@ -0,0 +1,21 @@
+
+public class Check {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public boolean hasDuplicateChars(String s) {
+		for(int i=0; i<s.length(); i++) {
+			for(int j=i+1; j<s.length(); j++) {
+				if(s) {
+					return false;
+					break;
+				}
+				else {
+					return true;
+				}
+			}
+		}
+	}
+}
